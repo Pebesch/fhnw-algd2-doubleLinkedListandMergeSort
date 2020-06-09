e5b7611c87f8db2284cc99b2d43f08a87218adb6
Peter Schmucki <pebs@gmx.ch>
2020-06-09 16:55:11 +0200

    Added Iterator

--- src/DoubleLinkedList/ComparableList.java
+++ src/DoubleLinkedList/ComparableList.java
@@ -1,5 +1,8 @@
 package DoubleLinkedList;
 
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
 public class ComparableList<E extends Comparable<E>> {
     private int size;
     private Element<E> head, tail;
@@ -61,6 +64,14 @@ public class ComparableList<E extends Comparable<E>> {
         }
     }
 
+    private void remove(Element<E> e) {
+        if (e != null && e != head && e != tail){
+            e.prev.next = e.next;
+            e.next.prev = e.prev;
+            size--;
+        }
+    }
+
     @Override
     public String toString(){
         String result = "";
@@ -81,6 +92,108 @@ public class ComparableList<E extends Comparable<E>> {
 
         public Element(E data) {
             this.data = data;
+        }
+    }
+
+    /* C List Iterator */
+    private class CListIterator implements ListIterator<E> {
+        // Last element returned
+        Element<E> returned;
+        int index;
+        // Next to index of iterator
+        Element<E> next;
+
+        public CListIterator() {
+            this.index = 0;
+            this.next = (Element<E>)head.next;
+            returned = null;
+        }
+
+        public CListIterator(CListIterator i){
+            next = i.next;
+            returned = null;
+            index = i.index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != next.next;
+        }
+
+        @Override
+        public E next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            returned = next;
+            next.next = next;
+            index++;
+            return returned.data;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return next.prev != next;
+        }
+
+        @Override
+        public E previous() {
+            if(!hasPrevious()) throw new NoSuchElementException();
+            returned = next.prev;
+            next = next.prev;
+            index--;
+            return returned.data;
+        }
+
+        @Override
+        public int nextIndex() {
+            if(hasNext()) {
+                return index + 1;
+            } else {
+                return size();
+            }
+        }
+
+        @Override
+        public int previousIndex() {
+            if(hasPrevious()) {
+                return index - 1;
+            } else {
+                return -1;
+            }
+        }
+
+        @Override
+        public void remove() {
+            if (returned == null) {
+                throw new IllegalStateException();
+            } else {
+                if (returned == next) {
+                    next = returned.next;
+                } else index--;
+                ComparableList.this.remove(returned);
+                returned = null;
+            }
+        }
+
+        @Override
+        public void set(E data){
+            if (returned == null)
+                throw new IllegalStateException();
+            else{
+                if (! (data instanceof Comparable<?>)){
+                    throw new IllegalArgumentException();
+                }
+                returned.data = data;
+            }
+        }
+
+        @Override
+        public void add(E e) {
+            Element<E> r = new Element<E>(e);
+            r.prev = next.prev;
+            r.next = next;
+            next.prev.next = r;
+            next.prev = r;
+            size++;
         }
     }
 }
